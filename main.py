673a2719ba77daa9c55ff3df3f5e89a1f3990e1e
Orion Ford <39802627+DesignisOrion@users.noreply.github.com>
2021-04-15 20:41:24 -0400

    adding files

---
+++ main.py
@@ -0,0 +1,6 @@
+import hashlib
+
+print(hashlib.sha256("DesignIsOrion is a taught developer from South Carolina. Enjoy this simple miner.".encode()).hexdigest())
+
+def mine(block_number, transactions, previous_hash):
+    pass
